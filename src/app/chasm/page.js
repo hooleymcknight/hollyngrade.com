d38a292ad6c2172c920ad67ebd6e88c285ccbf22
Holly Phillips <hollymphillips.dev@gmail.com>
2026-05-07 01:07:45 -0700

    started on a lightbox

---
+++ src/app/chasm/page.js
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from "react";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+
+
+import photos from '@/app/data/photos.json';
+import { Captions, Download, Share, Thumbnails, Video, Zoom } from "yet-another-react-lightbox/plugins";
+
+const format = [
+    {
+        "src": "/image1x3840.jpg",
+        "alt": "image 1",
+        "width": 3840,
+        "height": 2560,
+        "srcSet": [
+            { "src": "/image1x320.jpg", "width": 320, "height": 213 },
+            { "src": "/image1x640.jpg", "width": 640, "height": 427 },
+            { "src": "/image1x1200.jpg", "width": 1200, "height": 800 },
+            { "src": "/image1x2048.jpg", "width": 2048, "height": 1365 },
+            { "src": "/image1x3840.jpg", "width": 3840, "height": 2560 }
+        ]
+    }
+];
+
+export default function Chasm() {
+    const [open, setOpen] = useState(false);
+
+
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-center">
+            <div id="coming-soon" className="flex max-h-screen w-[95%] max-w-3xl flex-col items-center justify-between py-8 px-8 sm:px-16 bg-zinc-50 sm:items-start mt-8 mb-8"
+                style={{ background: "rgba(250, 250, 250, 0.6)" }}>
+                <div className="flex flex-row flex-wrap justify-center items-center gap-6 text-center sm:items-center sm:text-left sm:flex-nowrap">
+                    <button type="button" onClick={() => setOpen(true)}>
+                        Open Lightbox
+                    </button>
+
+                    <Lightbox
+                        plugins={[Captions, Download]}
+                        open={open}
+                        close={() => setOpen(false)}
+                        slides={photos}
+                    />
+                </div>
+            </div>
+        </main>
+    );
+}
