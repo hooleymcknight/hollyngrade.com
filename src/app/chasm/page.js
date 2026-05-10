747898bda6cc8f167ad951b1be5fc979d5f57f96
Holly Phillips <hollymphillips.dev@gmail.com>
2026-05-10 12:43:56 -0700

    added rest of lightbox plugins

--- src/app/chasm/page.js
+++ src/app/chasm/page.js
@@ -1,15 +1,42 @@
 'use client'
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
 
 
 import photos from '@/app/data/photos.json';
 import { Captions, Download, Share, Thumbnails, Video, Zoom } from "yet-another-react-lightbox/plugins";
 
+export default function Chasm() {
+    const [open, setOpen] = useState(true);
+
+    return (
+        <main className="flex flex-col min-h-screen items-center justify-center">
+            <div id="coming-soon" className="flex max-h-screen w-[95%] max-w-3xl flex-col items-center justify-between py-8 px-8 sm:px-16 bg-zinc-50 sm:items-start mt-8 mb-8"
+                style={{ background: "rgba(250, 250, 250, 0.6)" }}>
+                <div className="flex flex-row flex-wrap justify-center items-center gap-6 text-center sm:items-center sm:text-left sm:flex-nowrap">
+                    {/* <button type="button" onClick={() => setOpen(true)}>
+                        Open Lightbox
+                    </button> */}
+
+                    <Lightbox
+                        plugins={[Captions, Download, Share, Thumbnails, Video, Zoom]}
+                        open={open}
+                        close={() => setOpen(false)}
+                        slides={photos}
+                    />
+                </div>
+            </div>
+        </main>
+    );
+}
+
+/*
 const format = [
     {
         "src": "/image1x3840.jpg",
@@ -23,30 +50,18 @@ const format = [
             { "src": "/image1x2048.jpg", "width": 2048, "height": 1365 },
             { "src": "/image1x3840.jpg", "width": 3840, "height": 2560 }
         ]
-    }
+    },
+    {
+        type: "video",
+        width: 1280,
+        height: 720,
+        poster: "/public/poster-image.jpg",
+        sources: [
+            {
+                src: "/public/video.mp4",
+                type: "video/mp4",
+            },
+        ],
+    },
 ];
-
-export default function Chasm() {
-    const [open, setOpen] = useState(false);
-
-
-    return (
-        <main className="flex flex-col min-h-screen items-center justify-center">
-            <div id="coming-soon" className="flex max-h-screen w-[95%] max-w-3xl flex-col items-center justify-between py-8 px-8 sm:px-16 bg-zinc-50 sm:items-start mt-8 mb-8"
-                style={{ background: "rgba(250, 250, 250, 0.6)" }}>
-                <div className="flex flex-row flex-wrap justify-center items-center gap-6 text-center sm:items-center sm:text-left sm:flex-nowrap">
-                    <button type="button" onClick={() => setOpen(true)}>
-                        Open Lightbox
-                    </button>
-
-                    <Lightbox
-                        plugins={[Captions, Download]}
-                        open={open}
-                        close={() => setOpen(false)}
-                        slides={photos}
-                    />
-                </div>
-            </div>
-        </main>
-    );
-}
+*/
