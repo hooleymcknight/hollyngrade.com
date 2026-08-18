cdf343963ba35691958fed17037f8d265a0028a3
Holly M. Phillips <hollymphillips.dev@gmail.com>
2026-08-18 11:54:43 -0700

    ugh

--- src/app/bbq/page.js
+++ src/app/bbq/page.js
@@ -156,12 +156,12 @@ export default async function BbqPage({ searchParams }) {
               </li>
               <li><span className="act-name">Cornhole.</span> Set up in the yard.</li>
               <li>
-                <span className="act-name">Firepit, after dark.</span> <span>Evening-time friends, come by after
+                <span className="act-name" style={{ textDecoration: 'line-through' }}>Firepit, after dark.</span> <span style={{ textDecoration: 'line-through' }}>Evening-time friends, come by after
                 work &mdash; you won&rsquo;t have missed anything. Feel free to bring some s&rsquo;mores stuff; I&rsquo;ve got a few roasting sticks.</span>
                 <span
                     style={{ display: 'block', color: 'var(--red-deep)', marginTop: '3px', fontWeight: 'bold' }}
                 >
-                    &nbsp;&nbsp; &cross; &nbsp; No firepit, due to statewide burning ban through September 30
+                    &nbsp;&nbsp; ✗ &nbsp; No firepit, due to statewide burning ban through September 30
                     </span>
               </li>
             </ul>
